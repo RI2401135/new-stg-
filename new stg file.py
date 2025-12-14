6454e3ac9a674d22927df176163a9f1195ec4071
RI2401135 <RI2401135@tkh.edu.eg>
2025-12-14 02:34:03 +0200

    Update new stg file.py

--- new stg file.py
+++ new stg file.py
@@ -17,3 +17,34 @@ def get_message(max_bits):
         else:
             return bits
 
+def encode_message(image_path):
+    with open(image_path,'rb') as f:
+        file_content =f.read()
+
+    width=int.from_bytes(file_content[18:22], byteorder='little')
+    height=int.from_bytes(file_content[22:26],byteorder='little')
+    bytes_offset=int.from_bytes(file_content[10:14],byteorder='little')
+    bits_per_pixel=int.from_bytes(file_content[28:30],byteorder='little')
+    bytes_per_pixel= bits_per_pixel//8 
+    n= bytes_per_pixel
+
+    max_bits= width * height * n 
+    bits =get_message(max_bits)
+
+    file_content=list(file_content)
+    bits_index=0 
+    for i in range (bytes_offset, len(file_content),byteorder='little'):
+        for channel in range (n):
+            if bits_index <len(bits):
+                file_content[i + channel] =(file_content[i + channel]&~1) | int(bits[bit_index]) 
+                bits_index+=1
+            else:
+                break
+        if bit_index>=len(bits):
+            break
+    output_path="hidden_message.bmp"
+    with open(image_path,'wb') as f:
+        f.write(bytes(files_content))
+    print(f"Message hidden successfully in {output_path}")
+
+
