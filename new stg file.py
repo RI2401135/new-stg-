1621df1f9932bdc14995cfe422d98472045137ce
RI2401135 <RI2401135@tkh.edu.eg>
2025-12-14 02:18:16 +0200

    Update new stg file.py

--- new stg file.py
+++ new stg file.py
@@ -8,3 +8,12 @@ def get_image_path():
         except:
             print("error: image file not found or cannot be opened. please try again.")
 
+def get_message(max_bits):
+    while True:
+        message = input("Enter the secret message to hide:")
+        bits=''.join(format(ord(c),'08b') for c in message)+'00000000'
+        if len(bits)> max_bits:
+            print("Error: message too long for this image. Please enter a shorter message. ")
+        else:
+            return bits
+
